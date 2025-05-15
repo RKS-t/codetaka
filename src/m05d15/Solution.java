959d4bb2b637ae8343799c443be7d7e48682ceaf
RKS <rudtjs5719@gmail.com>
2025-05-15 23:04:29 +0900

    05-15 코드타카

---
+++ src/m05d15/Solution.java
@@ -0,0 +1,29 @@
+package m05d15;
+
+import java.util.Arrays;
+
+class Solution {
+    public String solution(String s) {
+
+        String[] numbers = s.split(" ");
+        int[] intNums = Arrays.stream(numbers).mapToInt(Integer::parseInt).toArray();
+
+        int[] result = {Math.min(intNums[0], intNums[1]), Math.max(intNums[0], intNums[1])};
+
+
+        for(int i = 2; i < numbers.length; i++){
+
+            if(result[0] > intNums[i]){
+                result[0] = intNums[i];
+            }
+
+            if(result[1] < intNums[i]){
+                result[1] = intNums[i];
+            }
+
+        }
+
+        String answer = result[0] + " " + result[1];
+        return answer;
+    }
+}
